1d1a1a214f49c297b2b1b9fa8e5eb651ec7d7495
jszablanowski <jan.szablanowski@gmail.com>
2021-12-03 00:29:06 +0100

    Add entities configuration

---
+++ src/Infrastructure/Persistence/Configurations/LoginConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infrastructure.Persistence.Configurations;
+
+public class LoginConfiguration : IEntityTypeConfiguration<Login>
+{
+    public void Configure(EntityTypeBuilder<Login> builder)
+    {
+        builder.Property(a => a.Email)
+            .IsRequired();
+    }
+}
