0673a3f1518e0af89273a677d63cf0b5e2d9c1c7
jszablanowski <jan.szablanowski@gmail.com>
2021-12-03 00:21:42 +0100

    Add MiniRentDbContext

---
+++ src/Application/Common/Interfaces/IMiniRentDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Common.Interfaces;
+
+public interface IMiniRentDbContext
+{
+    public DbSet<Login> Logins { get; }
+    public DbSet<Address> Addresss { get; }
+    public DbSet<CarModel> CarModels { get; }
+    public DbSet<Car> Cars { get; }
+    public DbSet<Company> Companys { get; }
+    public DbSet<Rent> Rents { get; }
+    public DbSet<Renter> Renters { get; }
+    public DbSet<Worker> Workers { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ src/Infrastructure/Persistence/MiniRentDbContext.cs
@@ -0,0 +1,96 @@
+﻿using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Domain.Common;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.ValueObjects;
+using CleanArchitecture.Infrastructure.Identity;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Entities;
+
+public class MiniRentDbContext : ApiAuthorizationDbContext<ApplicationUser>, IMiniRentDbContext
+{
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IDateTime _dateTime;
+    private readonly IDomainEventService _domainEventService;
+
+    private string _connectionString =
+        $"Data Source=dev-mini-rent-sqlserver.database.windows.net;Initial Catalog=RentalDB;Persist Security Info=True;" +
+        $"User ID=ef_migration;Password=Entity123";
+
+    public DbSet<Login> Logins => Set<Login>();
+    public DbSet<Address> Addresss => Set<Address>();
+    public DbSet<CarModel> CarModels => Set<CarModel>();
+    public DbSet<Car> Cars => Set<Car>();
+    public DbSet<Company> Companys => Set<Company>();
+    public DbSet<Rent> Rents => Set<Rent>();
+    public DbSet<Renter> Renters => Set<Renter>();
+    public DbSet<Worker> Workers => Set<Worker>();
+
+    public MiniRentDbContext(
+     DbContextOptions<MiniRentDbContext> options,
+     IOptions<OperationalStoreOptions> operationalStoreOptions,
+     ICurrentUserService currentUserService,
+     IDomainEventService domainEventService,
+     IDateTime dateTime) : base(options, operationalStoreOptions)
+    {
+        _currentUserService = currentUserService;
+        _domainEventService = domainEventService;
+        _dateTime = dateTime;
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedBy = _currentUserService.UserId;
+                    entry.Entity.Created = _dateTime.Now;
+                    break;
+
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedBy = _currentUserService.UserId;
+                    entry.Entity.LastModified = _dateTime.Now;
+                    break;
+            }
+        }
+
+        var events = ChangeTracker.Entries<IHasDomainEvent>()
+                .Select(x => x.Entity.DomainEvents)
+                .SelectMany(x => x)
+                .Where(domainEvent => !domainEvent.IsPublished)
+                .ToArray();
+
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await DispatchEvents(events);
+
+        return result;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    private async Task DispatchEvents(DomainEvent[] events)
+    {
+        foreach (var @event in events)
+        {
+            @event.IsPublished = true;
+            await _domainEventService.Publish(@event);
+        }
+    }
+}
