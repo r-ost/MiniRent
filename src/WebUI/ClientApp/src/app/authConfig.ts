b8517a0361a054c3c2efe61c8dbefb40819a14ba
jszablanowski <jan.szablanowski@gmail.com>
2022-01-29 11:38:43 +0100

    Change API_BASE_URL

--- src/WebUI/ClientApp/src/app/authConfig.ts
+++ src/WebUI/ClientApp/src/app/authConfig.ts
@@ -16,4 +16,4 @@ export const loginRequest = {
 };
 
 // Add the endpoints
-export const API_BASE_URL = process.env.API_BASE_URL;
+export const API_BASE_URL = "https://dev-api-mini-rent.azurewebsites.net";
